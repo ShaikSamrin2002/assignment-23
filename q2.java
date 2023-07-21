94a78ebd70e99e2f56d0619b7cb47a03ca9f2633
ShaikSamrin2002 <130962382+ShaikSamrin2002@users.noreply.github.com>
2023-07-21 12:15:49 +0530

    Update q2.java

--- q2.java
+++ q2.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class FindingMissingNumber {
     public static void main(String[] args) {
+         int n;  
+    Scanner sc=new Scanner(System.in);  
+    System.out.print("Enter the number of elements in array: ");  
+    n=sc.nextInt();  
+    int[] array = new int[10];  
+    System.out.println("Enter the elements of the array: ");  
+    for(int i=0; i<n; i++)  
+    {  
+    array[i]=sc.nextInt();  
+    }  
+    System.out.println("Array elements are: ");  
+    for (int i=0; i<n; i++)   
+    {  
+    System.out.println(array[i]);  
+    } 
  
-        System.out.println("Missing Number: " + findingMissingNumber(array));
+        System.out.println("Missing Number: " + FindingMissingNumber(array));
     }
 
     public static int FindingMissingNumber(int[] arr) {
